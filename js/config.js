7731ea999279c44a919c19aef0d390f8ad6f3c21
Desarrollo-Primee <230384@utags.edu.mx>
2026-07-10 14:10:09 -0600

    Update API_BASE_URL to include '/api' endpoint

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'https://express-r141co2al-justin15-xs-projects.vercel.app';
+export const API_BASE_URL = 'https://express-r141co2al-justin15-xs-projects.vercel.app/api';
